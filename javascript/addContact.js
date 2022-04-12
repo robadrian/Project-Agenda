56c13564ab0fe6b56980f9e6d5292953cf44fa51
robadrian_va <robadrian.va@gmail.com>
2022-04-12 22:47:36 +0300

    added taskModal

---
+++ javascript/addContact.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let conatctModal = `
+    <div id='contactDiv'> 
+        <h1 id='title'> NEW CONTACT </h1>
+        <input placeholder='Full Name' id='contactName'> 
+        <input placeholder='Phone Number' id='contactNumber' type='number'>
+        <button id='addBtn'> Add </button>
+    </div>
+`
+
+function addContact () {
+    const container = document.getElementById('work-area');
+    const text = document.getElementById('desc');
+    const taskDiv = document.getElementById('taskDiv')
+    const apointDiv = document.getElementById('apointDiv');
+    text !== null && container.removeChild(text);
+    taskDiv !== null && container.removeChild(taskDiv);
+    apointDiv !== null && container.removeChild(apointDiv);
+    container.insertAdjacentHTML("afterbegin" , conatctModal);
+    document.getElementById('addContact').disabled = true;
+    document.getElementById('addTask').disabled = false;
+    document.getElementById('addApoint').disabled = false;
+}
---
+++ javascript/addTask.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let taskModal = `
+    <div id='taskDiv'> 
+        <h1 id='title'> NEW TASK </h1>
+        <input placeholder='Title' id='taskTitle'> 
+        <input placeholder='Description' id='taskDescription' type='number'>
+        <button id='addBtn'> Add </button>
+    </div>
+`
+
+function addTask () {
+    const container = document.getElementById('work-area');
+    const text = document.getElementById('desc');
+    const contactDiv = document.getElementById('contactDiv')
+    const apointDiv = document.getElementById('apointDiv');
+    container.insertAdjacentHTML("afterbegin" , taskModal);
+    text !== null && container.removeChild(text);
+    contactDiv !== null && container.removeChild(contactDiv);
+    apointDiv !== null && container.removeChild(apointDiv);
+    document.getElementById('addTask').disabled = true;
+    document.getElementById('addContact').disabled = false;
+    document.getElementById('addApoint').disabled = false;
+}
